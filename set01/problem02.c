58b4dc52d56e1f17562afcce268db78d036747b6
ramayashreeg <ramayashreeg.bsc23@rvu.edu.in>
2024-01-23 09:55:58 +0530

    committed 1/23/2024, 9:55:58 AM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,3 +1,14 @@
 //2. Write a C program to add two numbers.
-
+#include <stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter the First number\n");
+    scanf("%d", &num1);
+    printf("Enter the Second Number\n");
+    scanf("%d", &num2);
+    sum = num1 + num2;
+    printf("The Sum of %d and %d is %d\n", num1, num2, sum);
+    return 0;
+}
 
