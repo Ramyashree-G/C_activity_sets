c6a78ddcdc0b1b7de4ea77a84c1d3d48f17e51c1
ramayashreeg <ramayashreeg.bsc23@rvu.edu.in>
2024-01-23 22:44:16 +0530

    test

--- set01/problem04.c
+++ set01/problem04.c
@@ -23,31 +23,27 @@ The sum of 1 and 2 is 3
 
 #include <stdio.h>
 
-void input()
+void input(int *a, int *b)
 {
     int num;
     printf("Enter the values for a and b:\n");
     scanf("%d", &num);
-    return num;
 }
 
-int add(int *a, int *b)
+void add(int a, int b, int *sum)
 {
-    int sum=0;
-    sum= a + b;
-    return sum;
+    *sum = a + b;
 }
 
-void output(int a, int b, int *sum)
+void output(int a, int b, int sum)
 {
     printf("The sum of %d and %d is %d", a,b,sum);
 }
 int main()
 {
     int a,b,sum;
-    a=input();
-    b=input();
-    sum=add(a,b);
+    input(&a ,&b);
+    add(a,b,&sum);
     output(a,b,sum);
     return 0;
 }
