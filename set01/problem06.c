bf31b5e5560b4919794dbf98bb63c7fbffb3e1f1
ramayashreeg <ramayashreeg.bsc23@rvu.edu.in>
2024-01-24 19:02:17 +0530

    problem07.c

--- set01/problem06.c
+++ set01/problem06.c
@@ -53,7 +53,7 @@ void compare(int a,int b,int c,int *largest)
     {
         *largest =a;
     }
-    else if(b>=a && b>=c)
+    else if(b>=c)
     {
         *largest =b;
     }
--- set01/problem07.c
+++ set01/problem07.c
@@ -23,8 +23,29 @@ The largest of 1,2 and 3 is 3.
 #include <stdio.h>
 int input_n()
 {
-    int num;
-    printf("Enter the numbers:\n");
-    scanf("%d", &num);
-    return num;
+    int n;
+    printf("Enter the number:\n");
+    scanf("%d", &n);
+    return n;
+}
+int sum_n_nos(int n)
+{
+    int i=0, sum=0;
+    for(i=1;i<=n;i++)
+    {
+        sum =sum+i;
+    }
+    return sum;
+}
+void output(int n, int sum)
+{
+    printf("The sum of first %d natural numbers is: %d\n", n,sum);
+}
+int main()
+{
+    int n,sum=0;
+    n= input_n();
+    sum= sum_n_nos(n);
+    output(n,sum);
+    return 0;
 }
