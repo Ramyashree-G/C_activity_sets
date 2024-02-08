353708ae4ce751cc022dd96ca0bb7be979c5a2d2
ramayashreeg <ramayashreeg.bsc23@rvu.edu.in>
2024-02-08 13:58:57 +0530

    test

--- set02/problem01.c
+++ set02/problem01.c
@@ -18,32 +18,29 @@ void output(float base, float height, float area);
 The area of the traingle with base 1.000000 and height 2.000000 is 1.000000*/
 
 #include <stdio.h>
-struct Triangle 
-{
-    float base;
-    float height;
-}
-typedef struct Triangle triangle
-
 void input(float *base, float *height)
 {
-    printf("Enter the Base for triangle");
-    scanf("%f", t.base);
-    printf("Enter the Height for triangle");
-    scanf("%f", t.height);
+    printf("Enter the Base for a Triangle\n");
+    scanf("%f", base);
+    printf("Enter the Height for a Triangle\n");
+    scanf("%f", height);
 }
 
 void find_area(float base , float height, float *area)
 {
-    area = 
+    *area= (0.5*(base*height));
 }
 
 void output(float base, float height, float area)
 {
-    printf("The area of the triangle with base %f and height %f is %f");
+    printf("The Area of Triangle with Base %f and Height %f is %f" , base, height, area);
 }
 
 int main()
 {
-
+    float base, height, area;
+    input(&base, &height);
+    find_area(base ,height, &area);
+    output(base, height, area);
+    return 0;
 }
---
+++ set02/problem02.c
@@ -0,0 +1,23 @@
+/*02.  Write a program to find if a triangle is scalene.
+
+> A triangle is scalene if all the three sides of the triangle are not equal to one another
+
+***Function Declarations***
+```c
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+```
+
+***Input***
+```
+5
+4
+5
+```
+
+***Output***
+```
+The triangle with sides 5, 4 and 5 is not scalene
+```
+*/
