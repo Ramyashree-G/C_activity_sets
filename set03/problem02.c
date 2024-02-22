5992713fa9b770673901ed69b8e91b9ee311dd2e
ramayashreeg <ramayashreeg.bsc23@rvu.edu.in>
2024-02-22 22:05:52 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -34,21 +34,40 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
     scanf("%f %f", x3, y3);
 }
 
-int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
 {
+    float dist1, dist2, dist3;
+    dist1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    dist2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+    dist3 = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
 
+    if ((dist1 + dist2 > dist3) && (dist2 + dist3 > dist1) && (dist3 + dist1 > dist2))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
-void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result)
 {
-
+    if (result)
+    {
+        printf("The points (%.1f, %.1f), (%.1f, %.1f) and (%.1f, %.1f) form a triangle\n", x1, y1, x2, y2, x3, y3);
+    }
+    else
+    {
+        printf("The points (%.1f, %.1f), (%.1f, %.1f) and (%.1f, %.1f) do not form a triangle\n", x1, y1, x2, y2, x3, y3);
+    }
 }
 
 int main()
 {
-    float x1,x2,x3,y1,y2,y3, result;
-    input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
-    is_triangle(x1,y1,x2,y2,x3,y3);
-    output(x1,y1,x2,y2,x3,y3,result);
+    float x1, x2, x3, y1, y2, y3, result;
+    input_triangle(&x1, &y1, &x2, &y2, &x3, &y3);
+    result = is_triangle(x1, y1, x2, y2, x3, y3);
+    output(x1, y1, x2, y2, x3, y3, result);
     return 0;
 }
